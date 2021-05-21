124eba62df571e5bd47e8b94b8406689f46ed88d
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-21 10:11:19 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-starter-sanity-blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "qzfyru8o",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
